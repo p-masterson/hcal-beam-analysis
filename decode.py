ee0f425122ea121cb88ae1b372d0b7f98e3c16d1
Einar Elén <einar.elen@gmail.com>
2021-10-20 17:42:10 +0200

    Update detectormap

--- decode.py
+++ decode.py
@@ -4,9 +4,9 @@ process.maxEvents=1
 process.outputFiles=['debug.root']
 
 from LDMX.Hcal import hgcrocFormat
-from LDMX.Hcal import HcalGeometry
+from LDMX.Hcal import DetectorMap
 
-hcal_det_map = HcalGeometry.HcalDetectorMap('testbeam_connections.csv')
+hcal_det_map = DetectorMap.HcalDetectorMap('${LDMX_BASE}/hcal/data/testbeam_connections.csv')
 hcal_decoder=hgcrocFormat.HcalRawDecoder('HcalDigis',
                                          input_file='sample_run206.raw',
                                          translate_eid=True) # this means use
