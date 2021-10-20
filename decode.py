f82f11e505a821c6ce2988f4fad8986271bfd39e
Einar Elén <einar.elen@gmail.com>
2021-10-20 16:25:27 +0200

    Updated decoding script

--- decode.py
+++ decode.py
@@ -4,6 +4,9 @@ process.maxEvents=1
 process.outputFiles=['debug.root']
 
 from LDMX.Hcal import hgcrocFormat
+from LDMX.Hcal import HcalGeometry
+
+hcal_det_map = HcalGeometry.HcalDetectorMap('testbeam_connections.csv')
 hcal_decoder=hgcrocFormat.HcalRawDecoder('HcalDigis',
                                          input_file='sample_run206.raw',
                                          translate_eid=True) # this means use
