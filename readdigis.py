9ce1460328ddccab703d1a0c83210b4542faea82
Einar Elén <einar.elen@gmail.com>
2021-10-21 17:30:37 +0200

    Simple script reading reco

---
+++ readdigis.py
@@ -0,0 +1,15 @@
+
+import ROOT
+from ROOT import TCanvas, TPad, TFile, TPaveLabel, TPaveText, TStyle, TTree, TH1D, TH2D, TLegend, TGraph, TGraphErrors
+from ROOT import gROOT, gStyle, gSystem, gPad
+gSystem.Load("libFramework.so")
+from TranslateHcalID import HcalDigiID, bar_to_pos
+
+inputFile=TFile("debug.root", "READ")
+eventTree=inputFile.Get("LDMX_Events")
+
+
+
+for event in eventTree:
+    digis = event.HcalDigis_test
+    print(digis.getNumSamplesPerDigi())
