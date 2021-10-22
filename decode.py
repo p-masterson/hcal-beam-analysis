7722eed17a89ed17ce32ed8607f97c91b1162f5b
Einar Elén <einar.elen@gmail.com>
2021-10-22 11:23:01 +0200

    Use CSV instead of hardcoded vales

--- decode.py
+++ decode.py
@@ -6,11 +6,37 @@ process.outputFiles=['debug.root']
 from LDMX.Hcal import hgcrocFormat
 from LDMX.Hcal import DetectorMap
 
-hcal_det_map = DetectorMap.HcalDetectorMap('${LDMX_BASE}/hcal/data/testbeam_connections.csv')
-hcal_decoder=hgcrocFormat.HcalRawDecoder('HcalDigis',
-                                         input_file='sample_run206.raw',
+from TranslateHcalID import HcalDigiID, bar_to_pos, HcalID
+
+from LDMX.Conditions.SimpleCSVTableProvider import SimpleCSVDoubleTableProvider
+
+HcalReconConditionsDummy=SimpleCSVDoubleTableProvider('HcalReconConditions',
+                                                     ["ADC_PEDESTAL",
+                                                      "ADC_GAIN",
+                                                      "TOT_PEDESTAL",
+                                                      "TOT_GAIN"])
+HcalReconConditionsDummy.validForever('./DumbReconConditions.csv')
+
+from LDMX.Hcal import HcalGeometry
+from LDMX.Packing import rawio
+
+reader=rawio.SingleSubsystemUnpacker(raw_file='run208.raw', output_name='HcalRaw',
+                                     num_words_per_event=1026, bytes_per_word=4,
+                                     detector_name='ldmx-hcal-prototype-v1.0')
+det_map=DetectorMap.HcalDetectorMap('${LDMX_BASE}/ldmx-sw/Hcal/data/testbeam_connections.csv')
+# Note: I'm calling the output HcalDigis instead of EIDHcalDigis because we are
+# making the translation from electronics IDs to DigiIDs
+
+
+hcal_decoder=hgcrocFormat.HcalRawDecoder(output_name='HcalDigis',
+                                         input_name='HcalRaw',
+                                         input_file='run208.raw',
                                          translate_eid=True) # this means use
                                                              # the
                                                              # HcalDetectorMap
 
-process.sequence=[hcal_decoder]
+
+from LDMX.Hcal import digi
+hcal_recs=digi.HcalRecProducer()
+hcal_recs.digiCollName='HcalDigis'
+process.sequence=[reader,hcal_decoder,hcal_recs]
